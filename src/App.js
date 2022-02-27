8d17a639efff3698bd256e69e0f72444e0f1efd3
Prince <77054941+Prince4347@users.noreply.github.com>
2022-02-28 03:38:49 +0500

    Update App.js
    togrlandi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Home from './page/home';
-import Card from './containers/cards/index.jsx'
-import Local from './containers/local';
 
 function App() {
   return (
     <div >
       <Home /> 
-      <Card />
-      <Local/>
     </div>
   );
 }

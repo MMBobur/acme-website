48f9a8690dff02df5fd2bdc7b47657f4dda158b7
AzamatjonFrontEnd <my.githubprofile@gmail.com>
2022-02-27 14:25:58 +0500

    Banner Complated

---
+++ src/containers/banner/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const banner = () => {
+  return (
+    <div className="banner">
+      <h1>Serving you <br /> since 1989.</h1>
+      <p>Acme Outdoors is an outdoor and adventure <br /> shop located in the Boathouse District in <br /> Oklahoma City.</p>
+      <button className="btn">Shop Merch</button>
+    </div>
+  )
+}
+
+export default banner;

dad407c67808d3a9bc30053cf90fb71cc4e3b699
RegnumUz <regnum1308@gmail.com>
2022-02-28 02:33:48 +0500

    cards

---
+++ src/containers/cards/cards.js
@@ -0,0 +1,19 @@
+const CardsData = [
+  {
+    id: "01",
+    title: "SHOP PRODUCTS",
+    text: "Our full product line is still available online here on our site! Getting outside and hiking is still something you can do. Get your gear now!",
+  },
+  {
+    id: "02",
+    title: "DONATE",
+    text: "Since we've changed the way we operate to online only, and to ensure your safety, not all our staff is working. Donate to keep them afloat.",
+  },
+  {
+    id: "03",
+    title: "BUY GIFT CARDS",
+    text: "Have all the outdoor gear you need for now? Buy a gift card and use it later or share it with friends and family.",
+  },
+];
+
+export default CardsData;

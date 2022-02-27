192e216e804599aab19bb164f61d91af7e5c8545
RegnumUz <regnum1308@gmail.com>
2022-02-27 11:14:20 +0500

    'cards'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './page/home';
+import Card from './containers/cards/index.jsx'
 
 function App() {
   return (
     <div >
       <Home /> 
+      <Card />
     </div>
   );
 }

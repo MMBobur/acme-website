9932e15f74f39bc835e92cca9a9dda406745332d
Prince <princeuzgang@gmail.com>
2022-03-01 00:42:19 +0500

    done

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Cards from '../containers/cards/index.jsx';
-import Footer from '../components/footer/index.jsx';
-import Covid from '../containers/covid/index.jsx'
-import Keep from '../containers/keeping/index.jsx'
+import Navbar from "../containers/navbar/index.jsx";
+import Banner from "../containers/banner/index.jsx";
+import Cards from "../containers/cards/index.jsx";
+import Covid from "../containers/covid/index.jsx";
+import Keep from "../containers/keeping/index.jsx";
+// import Products from "../containers/products/index.jsx";
+import Local from "../containers/local/index.jsx";
+import Footer from "../components/footer/index.jsx";
 
 function Home() {
   return (
     <div>
+      <Navbar />
+      <Banner />
       <Covid />
       <Cards />
       <Keep />
+      {/* <Products /> */}
+      <Local />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

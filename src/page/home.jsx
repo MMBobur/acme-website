6700f6bf0351c1d491b15efd4cb5930294765ee3
XayrulloDev2005 <xayrullodev@gmail.com>
2022-02-28 16:00:19 +0500

    keep papkani ozi

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Covid from '../containers/covid/index.jsx'
 import Cards from '../containers/cards/index.jsx' 
 import Footer from '../containers/footer/index.jsx'
-
+import Keep from '../containers/keeping/index.jsx'
 function Home() {
   return (
     <div>
       <Covid />
       <Cards />
+      <Keep />
       <Footer />
     </div>
   )
